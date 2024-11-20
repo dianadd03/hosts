ce7eb4d4b650f65a753b25a37523e3860b8dedf5
Dorneanu Diana-Delia <diana-delia.dorneanu@s.unibuc.ro>
2024-11-20 17:36:43 +0000

    Primul commit: Script pentru validarea adreselor IP din /etc/hosts

---
+++ ex3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat /etc/hosts | while read -r line
+do  
+  ip=$(echo "$line" | awk '{print $1}')
+  domeniu=$(echo "$line" | awk '{print $2}')
+  
+  ip2=$(nslookup "$domeniu" | grep "Address" | tail -n 1 | awk '{print $2}')
+  
+  if [[ "$ip" != "$ip2" ]]; then
+    echo "Bogus IP for $domeniu in /etc/hosts!"
+  fi
+done
