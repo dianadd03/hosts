82b63934da9d6df9088a580a0b2443a0fabf6318
Dorneanu Diana-Delia <diana-delia.dorneanu@s.unibuc.ro>
2024-11-20 17:39:43 +0000

    Al doilea commit: Script actualizat cu funcție shell pentru verificarea IP-urilor și hostname

--- ex3.sh
+++ ex3.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-cat /etc/hosts | while read -r line
-do  
-  ip=$(echo "$line" | awk '{print $1}')
-  domeniu=$(echo "$line" | awk '{print $2}')
-  
-  ip2=$(nslookup "$domeniu" | grep "Address" | tail -n 1 | awk '{print $2}')
-  
-  if [[ "$ip" != "$ip2" ]]; then
-    echo "Bogus IP for $domeniu in /etc/hosts!"
-  fi
-done
+check_ip_host() {
+    local hostname=$1
+    local ip=$2
+    local dns_server=$3
+
+        if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Valid IP format: $ip"
+    else
+        echo "Invalid IP format: $ip"
+        return 1
+    fi
+
+        resolved_ip=$(dig +short "$hostname" @"$dns_server")
+    if [[ $resolved_ip == "$ip" ]]; then
+        echo "Hostname $hostname și IP $ip sunt corect asociate."
+    else
+        echo "Asociere greșită pentru $hostname și $ip."
+    fi
+}
+
+while read -r line; do
+    ip=$(echo "$line" | awk '{print $1}')
+    hostname=$(echo "$line" | awk '{print $2}')
+    check_ip_host "$hostname" "$ip" "8.8.8.8"
+done < /etc/hosts
